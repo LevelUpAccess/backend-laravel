a16c3be4fbed99b8a2cb231c722a14c203062e0b
Andrea Lucero <alucerotrasvina@gmail.com>
2024-05-29 17:32:14 -0700

    reglas de validacion del registro

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ProductoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('/categorias', CategoriaController::class);
+Route::apiResource('/productos', ProductoController::class);
+
+//Autenticacion
+Route::post('/registro', [AuthController::class, 'register']);
