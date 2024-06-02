0a916210c53ff57900000d5c574df3125ed521fe
Andrea Lucero <alucerotrasvina@gmail.com>
2024-06-01 17:54:33 -0700

    login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegistroRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -27,8 +29,22 @@ class AuthController extends Controller
         ];
     }
     
-    public function login(Request $request) {
-        
+    public function login(LoginRequest $request) {
+        $data = $request->validated();
+
+        //Revisar el password
+        if(!Auth::attempt($data)) {
+            return response([
+                'errors' => ['El email o password son incorrectos']
+            ], 422);
+        }
+
+        //Autenticar al usuario
+        $user = Auth::user();
+        return [
+            'token' => $user->createToken('token')->plainTextToken,
+            'user' => $user
+        ];
     }
 
     public function logout(Request $request) {
